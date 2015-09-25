d5671e772500c3dbb16e32c2e6be90e3fe18197c
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2015-09-25 09:43:06 -0700

    first commit

---
+++ reinstallpacks.R
@@ -0,0 +1,14 @@
+## instead of being smart, I'll be quick.
+## all the packages I'll need to re-install after ever updating r:
+
+## btw:
+## brew update
+## brew upgrade r
+
+packages <- c("ggplot2","dplyr","plyr","gridExtra","lme4", "MCMCglmm", "lazyeval", "rjags", "coda", "digest", "xlsx", "stringdist", "DBI", "devtools", "data.table", "e1071", "reshape", "foreach", "rJava", "RSQLite", "sqldf", "data.table", "rmarkdown", "stargazer", "pryr", "chron", "readr", "R.matlab")
+
+lapply(packages, install.packages)
+
+## Notes:
+## if rJava is giving you problems, make sure the dev kit is installed:
+## run: R CMD javareconf
