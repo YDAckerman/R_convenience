e3f9eaf14efebb13641250dd228c416386e58765
YDAckerman <jonathan.ackerman0@gmail.com>
2016-01-05 17:18:28 -0800

    updated helper functions and package reinstall script

--- helper_functions.R
+++ helper_functions.R
@@ -7,23 +7,27 @@ hf <- new.env()
 ## a quick and dirty multi-grepl function:
 ## wow, can easily do this with regex...?
 hf$mgrepl <- function(patterns, x, ignore.case = FALSE,
-                      strict = 0, fuzzy = FALSE){
+                      strict = 0, fuzzy = FALSE, perl = FALSE){
     require(plyr)
     f <- if (fuzzy){ agrepl } else { grepl }
     bool_df <- ldply(patterns, function(pattern){
-        f(pattern, x, ignore.case = ignore.case)
+        f(pattern, x, ignore.case = ignore.case, perl = perl)
     })
     colSums(bool_df) > strict
 }
 
 # a quicker and dirtier multi-grep function:
 hf$mgrep <- function(patterns, x, ignore.case = FALSE,
-                     strict = 0, fuzzy = FALSE){
+                     strict = 0, fuzzy = FALSE, value = FALSE, perl = FALSE){
 
     i <- hf$mgrepl(patterns, x, ignore.case = ignore.case,
-              strict = strict, fuzzy = fuzzy)
-    
-    seq_along(x)[i]
+              strict = strict, fuzzy = fuzzy, perl = perl)
+
+    if(value){
+        x[i]
+    } else {
+        seq_along(x)[i]        
+    }
 }
 
 ## trail leading and trailing whitespace:
@@ -82,6 +86,62 @@ hf$SegmentVec <- function(vec, num.segs, ordered = TRUE){
     return(segments)
 }
 
+## insert new values into specified indices in a
+## vector
+hf$insert.vals <- function(vector, values, indices){
+    if(length(values) != length(indices)){
+        values <- rep(values, length(indices))
+    }
+    indices <- sort(indices)
+    vector <- c(
+        vector[1:(indices[1] - 1)],
+        values[1],
+        vector[indices[1]:length(vector)]
+    )
+    if(length(indices) != 1){
+        indices <- indices[2:length(indices)]
+        values <- values[2:length(values)]
+        vector <- insert.vals(vector, values, indices)
+    }
+    vector
+}
+
+hf$convertMonthNamesToNumeric <- function(mo_name){
+   
+    i <- 0
+    for (month in months){
+        i <- i + 1
+        if(grepl(month, mo_name, ignore.case = TRUE)){
+            return(i)
+        }
+    }
+    return(NA)
+}
+
+hf$mgsub <- function(pattern, replacement, x, ...) {
+  if (length(pattern)!=length(replacement)) {
+    stop("pattern and replacement do not have the same length.")
+  }
+  result <- x
+  for (i in 1:length(pattern)) {
+    result <- gsub(pattern[i], replacement[i], result, ...)
+  }
+  result
+}
+
+hf$fixDates <- function(date, format = "%d/%m/%Y"){
+    months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+    as.Date(gsub("/0", "/200",
+                 hf$mgsub(c(months, "-", " ", "208", "some subset of:", "dates not specified"),
+                          c(1:12, "/", "/", "2008", "", ""),
+                          hf$trim(hf$removeParens(date)))
+                 ),
+            format = format)
+}
+
+hf$symdiff <- function(A, B){
+    c(setdiff(A, B), setdiff(B, A))
+}
 
 ############################################################
 ############################################################
--- reinstallpacks.R
+++ reinstallpacks.R
@@ -7,7 +7,16 @@
 
 packages <- c("ggplot2","dplyr","plyr","gridExtra","lme4", "MCMCglmm", "lazyeval", "rjags", "coda", "digest", "xlsx", "stringdist", "DBI", "devtools", "data.table", "e1071", "reshape", "foreach", "rJava", "RSQLite", "sqldf", "data.table", "rmarkdown", "stargazer", "pryr", "chron", "readr", "R.matlab", "stringr", "orthopolynom")
 
-lapply(packages, install.packages)
+choice <- readline(prompt = "update or install: ")
+while(choice != "update" & choice != "install"){
+    choice <- readline(prompt = "please enter 'update' or 'install': ")
+}
+
+f <- switch(choice,
+            update = update.packages,
+            install = install.packages)
+
+all_null <- lapply(packages, f)
 
 ## Notes:
 ## if rJava is giving you problems, make sure the dev kit is installed:
